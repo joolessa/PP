098fbf9d62b0ae3835d01b75e71fc5a0344e1c28
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-06 23:42:30 +0000

    Geração de tela com fundo

---
+++ Jogo Inicial.py
@@ -0,0 +1,24 @@
+import pygame
+
+# Iniciando Tela
+pygame.init()
+tela = pygame.display.set_mode((1152,800))
+pygame.display.set_caption('THE ERAS GAME')
+tempo = pygame.time.Clock()
+
+# IMAGENS
+imagem_inicial = pygame.image.load('Imagens/fundo_inicial.jpeg')
+
+# LOOP PRINCIPAL
+game = True
+while game:
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+            game = False
+    tela.blit(imagem_inicial, (0,0))
+
+    pygame.display.update()
+    tempo.tick(60)
+
+# FINALIZAÇÃO
+pygame.quit()
