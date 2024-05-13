00c7fc8900d7abf59b3f5e5cb316928c3dffd253
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-13 16:37:36 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -6,7 +6,7 @@ import numpy as np
 l_personagem = 50
 h_personagem = 50
 
-minion = pygame.image.load('onde está a imagem')
+minion = pygame.image.load('Imagens/Minion Bob.png')
 pygame.Rect(
         TAMANHO_DA_TELA[0] // 4, TAMANHO_DA_TELA[1] - ALTURA_PERSONAGEM, LARGURA_PERSONAGEM, ALTURA_PERSONAGEM
     )
