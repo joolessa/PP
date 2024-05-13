267e9c41e7da965462af8880991a51c374662a71
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-13 16:33:49 -0300

    Update personagem.py
    como colocar a imagem do GitHub?

--- personagem.py
+++ personagem.py
@@ -2,5 +2,11 @@ import pygame
 import sounddevice as sd
 import numpy as np
 
-LARGURA_PERSONAGEM = 50
-ALTURA_PERSONAGEM = 50
+
+l_personagem = 50
+h_personagem = 50
+
+minion = pygame.image.load('onde está a imagem')
+pygame.Rect(
+        TAMANHO_DA_TELA[0] // 4, TAMANHO_DA_TELA[1] - ALTURA_PERSONAGEM, LARGURA_PERSONAGEM, ALTURA_PERSONAGEM
+    )
