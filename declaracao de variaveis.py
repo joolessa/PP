5120ecea8433820d90e556dca4b622d9d71c600e
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-13 17:59:56 -0300

    Update declaracao de variaveis.py
    ajuste

--- declaracao de variaveis.py
+++ declaracao de variaveis.py
@@ -1,11 +1,7 @@
-# Constantes para o jogo
 tamanho_tela = (1300,800)
 largura_personagem = 50
 altura_personagem = 50
 
-# Gravidade quanto maior o valor mais rapido o personagem cai
-gravidade = 3
-# Sensibilidade do som é o quanto o som precisa ser alto para o personagem subir
-sensi_som = 1
-# Amortecimento do som quanto maior o valor mais rapido o personagem para de cair
-amortecimento = 0.95
+gravidade = 3 # Gravidade quanto maior o valor mais rapido o personagem cai
+sensi_som = 1 # quanto o som precisa ser alto para o personagem subir
+amortecimento = 0.95 # Amortecimento do som quanto maior o valor mais rapido o personagem para de cair
