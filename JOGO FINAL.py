c35d9b8b22dcd0b4218e15dedbb139197f46331d
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-13 17:59:30 -0300

    Update JOGO FINAL.py
    declaração de variáveis

--- JOGO FINAL.py
+++ JOGO FINAL.py
@@ -2,3 +2,12 @@
 import pygame
 import sounddevice as sd
 import numpy as np
+
+# DECLARAÇÃO DE VARIÁVEIS
+tamanho_tela = (1300,800)
+largura_personagem = 50
+altura_personagem = 50
+
+gravidade = 3 # Gravidade quanto maior o valor mais rapido o personagem cai
+sensi_som = 1 # quanto o som precisa ser alto para o personagem subir
+amortecimento = 0.95 # Amortecimento do som quanto maior o valor mais rapido o personagem para de cair
