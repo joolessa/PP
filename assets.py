741f6c3ab268f6da9c88f8f2ff1b624f4c6dbed0
Joana Lessa <166643357+joolessa@users.noreply.github.com>
2024-05-23 01:48:34 +0000

    ajuste explosao

--- assets.py
+++ assets.py
@@ -9,11 +9,11 @@ def load_assets():
     assets['piso'] = pygame.image.load('Imagens/piso.PNG')
     assets['teto'] = pygame.image.load('Imagens/teto.PNG')
     assets['espinhos'] = pygame.image.load('Imagens/obstaculo.png')
-    assets['exp1'] = pygame.image.load('Imagens/exp1.png')
-    assets['exp2'] = pygame.image.load('Imagens/exp2.png')
-    assets['exp3'] = pygame.image.load('Imagens/exp3.png')
-    assets['exp4'] = pygame.image.load('Imagens/exp4.png')
-    assets['exp5'] = pygame.image.load('Imagens/exp5.png')
-    exp_anim = [assets['exp1'],assets['exp2'], assets['exp3'], assets['exp4'], assets['exp5']]
-
-    return (assets, exp_anim)
+    assets['exp_anim'] = [
+    pygame.image.load('Imagens/exp1.png'),
+    pygame.image.load('Imagens/exp2.png'),
+    pygame.image.load('Imagens/exp3.png'),
+    pygame.image.load('Imagens/exp4.png'),
+    pygame.image.load('Imagens/exp5.png')
+]
+    return assets
--- explosao.py
+++ explosao.py
@@ -1,28 +1,28 @@
 import pygame
 from pygame.sprite import Group
-from assets import *
+from assets import load_assets
 
 # Código retirado do seguinte link: https://www.youtube.com/watch?v=d06aVDzOfV8
 class Explosion(pygame.sprite.Sprite):
     def __init__(self, assets, center):
         pygame.sprite.Sprite.__init__(self)
-        self.explosion_anim = exp_anim
-        self.image = self.explosion_anim
+        self.exp_anim = assets['exp_anim']
+        self.image = self.exp_anim
         self.rect = self.image.get_rect()
         self.rect.center = center
         self.frame = 0
         self.last_update = pygame.time.get_ticks()
-        self.frame_rate = 50  # Velocidade da animação
+        self.frame_rate = 50 
 
     def update(self):
         now = pygame.time.get_ticks()
         if now - self.last_update > self.frame_rate:
             self.last_update = now
             self.frame += 1
-            if self.frame == len(self.explosion_anim):  # Supondo que `explosion_anim` seja uma lista de frames
+            if self.frame == len(self.exp_anim): 
                 self.kill()
             else:
                 center = self.rect.center
-                self.image = self.explosion_anim[self.frame]
+                self.image = self.exp_anim[self.frame]
                 self.rect = self.image.get_rect()
                 self.rect.center = center
