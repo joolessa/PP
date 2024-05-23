96f46826815dd3b1fe3adee57082de65ccbbb617
valen-meira <valentinasm@al.insper.edu.br>
2024-05-22 22:50:28 -0300

    instruções tela inicial

--- tela_inicial.py
+++ tela_inicial.py
@@ -13,12 +13,16 @@ def tela_inicial(tela):
     # Renderize o texto desejado
     texto1 = fonte.render("Bem Vindo ao PP game!", True, branco)
     texto2 = fonte.render("Clique para iniciar e solte sua voz!", True, branco)
-    texto3 = fonte.render("Feito por Joana, Maria e Valentina", True, branco)
+    texto3 = fonte.render("Bob, o minion, moverá para cima ou para baixo de acordo com a sua voz!", True, branco)
+    texto4 = fonte.render("Controle seu vozerão para não deixar ele tocar nos espinhos do teto, do piso e os que estão vindo em sua direção da direita!", True, branco)
+    texto5 = fonte.render("Feito por Joana e Valentina", True, branco)
 
     # Posicione o texto no centro da tela
-    pos_texto1 = texto1.get_rect(center=((width// 2), (height // 2)-200))
-    pos_texto2 = texto2.get_rect(center=((width// 2)-10, (height // 2)-100))
-    pos_texto3 = texto3.get_rect(center=((width// 2)-10, (height // 2)))
+    pos_texto1 = texto1.get_rect(center=((width// 2), (height // 2)-300))
+    pos_texto2 = texto2.get_rect(center=((width// 2)-10, (height // 2)+100)
+    pos_texto3 = texto4.get_rect(center=((width// 2)-10, (height // 2)-200))
+    pos_texto4 = texto4.get_rect(center=((width// 2)-10, (height // 2)-100))
+    pos_texto5 = texto4.get_rect(center=((width// 2)-10, (height // 2)+350))
 
     #trecho extraido de https://dessoft.insper-comp.com.br/conteudo/projeto
     running = True
@@ -39,6 +43,9 @@ def tela_inicial(tela):
 
         tela.blit(texto1, pos_texto1)
         tela.blit(texto2, pos_texto2)
+        tela.blit(texto3, pos_texto3)
+        tela.blit(texto4, pos_texto4)
+        tela.blit(texto5, pos_texto5)
 
         pygame.display.flip()
 
